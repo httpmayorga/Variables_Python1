4e53c673ee7f169e7a5673a6e8db48a0b7a461e7
Andres J. Mayorga <httpmayorga@gmail.com>
2023-08-24 16:14:56 -0500

    Primer trabajo convariables python

---
+++ Python_Variables.py
@@ -0,0 +1,21 @@
+# Definir variables de diferentes tipos
+cadena = "Hola, este es un"
+entero = 10
+flotante = 3.14159
+Booleano = True
+# Concatenar variables a la cadena
+
+resultado = cadena + " ejemplo." + " El entero es: " + str(entero) + " y el flotante es: " + str(flotante) + " y el boleano es: " +str(Booleano)
+# Límites de enteros y flotantes en Python
+""""entero_maximo = 2**31 - 1
+entero_minimo = -2**31
+flotante_maximo = 1.8e308
+flotante_minimo = 2.2e-308 """""
+
+# Fórmula de la suma de los primeros n números pares
+n = entero
+suma_pares = n * (n + 1)
+
+# Imprimir resultados
+print(resultado)
+print("Suma de los primeros", n, "números pares:", suma_pares /n)
